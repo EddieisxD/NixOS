240711edcc682c0d104b32bb4ae3127d759994e6
EddieisxD <eddieisxd12@gmail.com>
2026-03-17 20:05:23 +0530

    prolly fixed the wifi issue by adding proprietary drivers in the system

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -10,5 +10,6 @@
   boot.extraModulePackages = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.enableRedistributableFirmware = true;
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
