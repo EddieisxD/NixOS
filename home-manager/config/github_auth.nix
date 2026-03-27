a8f9e3d28b82dd2ba9ca0a29c630dc4f159b9e2c
EddieisxD <eddieisxd12@gmail.com>
2026-03-27 07:30:16 +0530

    made github auth more robust

--- home-manager/config/github_auth.nix
+++ home-manager/config/github_auth.nix
@@ -8,11 +8,13 @@
         hostname = "github.com";
         user = "git";
         identityFile = "~/.ssh/id_github_aditya22598";
+        identitiesOnly = true;
       };
       "github_eddieisxd12" = {
         hostname = "github.com";
         user = "git";
         identityFile = "~/.ssh/id_github_eddieisxd12";
+        identitiesOnly = true;
       };
     };
   };
