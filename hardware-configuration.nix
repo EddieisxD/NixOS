b2a8e8a4410d38cde93aafce3e1aa126513407c6
EddieisxD <eddieisxd12@gmail.com>
2026-03-29 03:36:12 +0530

    added the modulesPath import back in the hardware-configuration.nix

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -1,6 +1,9 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
+  imports = [
+      (modulesPath + "/installer/scan/not-detected.nix")
+  ];
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "vmd" "usbhid" "usb_storage" "sd_mod" "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
