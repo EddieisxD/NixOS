086db991d9c1e526e590d934b53f4b70d20fd5df
EddieisxD <eddieisxd12@gmail.com>
2026-06-24 01:42:41 +0530

    cuda maintainers new cache i.e. from legacy version on cachix -> nixos

--- modifications.nix
+++ modifications.nix
@@ -171,15 +171,15 @@
       substituters = [
 	      "https://cache.nixos.org"
 	      "https://nix-community.cachix.org"
-	      "https://devenv.cachix.org"
-        "https://cuda-maintainers.cachix.org"
+	      "https://cache.nixos-cuda.org"
+        "https://devenv.cachix.org"
       ];
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
 	      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
 	      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       ];
 
 
