9a0d3095ca7ff66b98c6f086b4667b6b8efd95be
EddieisxD <eddieisxd12@gmail.com>
2026-06-27 01:44:32 +0530

    added cuda maintainers cachix just to be sure + system update

--- modifications.nix
+++ modifications.nix
@@ -202,6 +202,7 @@
 	      "https://nix-community.cachix.org"
 	      "https://cache.nixos-cuda.org"
         "https://devenv.cachix.org"
+        "https://cuda-maintainers.cachix.org"
       ];
 
       trusted-public-keys = [
@@ -209,6 +210,7 @@
 	      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
 	      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       ];
 
 
